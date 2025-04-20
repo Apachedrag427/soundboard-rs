7f8ea0c390d4d49376a2e9807ac3d7e2292d7558
Apache <apache.software.foundation@gmail.com>
2025-04-19 20:20:07 -0500

    No more hardcoded sound file

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use kira::{
 use std::collections::HashMap;
 use std::thread;
 use std::sync::mpsc;
+use std::fs;
 
 enum AudioAction {
 	Play(String),
@@ -19,6 +20,17 @@ fn main() -> eframe::Result {
 		viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
 		..Default::default()
 	};
+	if !fs::exists("./Audios").unwrap() || !fs::metadata("./Audios").unwrap().is_dir() {
+		fs::create_dir("./Audios").unwrap();
+	}
+
+	let mut audio_files: Vec<String> = Vec::new();
+	let paths = fs::read_dir("./Audios").unwrap();
+
+	for path in paths {
+		audio_files.push(path.unwrap().file_name().into_string().unwrap());
+	}
+
 	let (tx, rx) = mpsc::channel();
 	thread::spawn(move || {
 		let mut cache: HashMap<String, StaticSoundData> = HashMap::new();
@@ -45,18 +57,19 @@ fn main() -> eframe::Result {
 		"soundboard-rs",
 		options,
 		Box::new(|_cc| {
-			Ok(Soundboard::with_transmitter(tx))
+			Ok(Soundboard::new(tx, audio_files))
 		}),
 	)
 }
 
 struct Soundboard {
 	transmitter: mpsc::Sender<AudioAction>,
+	audio_files: Vec<String>
 }
 
 impl Soundboard {
-	fn with_transmitter(transmitter: mpsc::Sender<AudioAction>) -> Box<Self> {
-		Box::new(Self { transmitter })
+	fn new(transmitter: mpsc::Sender<AudioAction>, audio_files: Vec<String>) -> Box<Self> {
+		Box::new(Self { transmitter, audio_files })
 	}
 }
 
@@ -64,9 +77,13 @@ impl eframe::App for Soundboard {
 	fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
 		egui::CentralPanel::default().show(ctx, |ui| {
 			ui.heading("Soundboard");
-			if ui.button("Play").clicked() {
-				self.transmitter.send(AudioAction::Play(String::from("are-you-sure.mp3"))).unwrap();
-			}
+			egui::ScrollArea::vertical().show(ui, |ui| {
+				for file in &self.audio_files {
+					if ui.button(file).clicked() {
+						self.transmitter.send(AudioAction::Play(file.to_owned())).unwrap();
+					}
+				}
+			});
 		});
 	}
 }
